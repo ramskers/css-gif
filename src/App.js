12c4936bc8fb226c12e86a343cd733768d0fca5b
Your Name <you@example.com>
2022-08-29 15:39:05 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="one">
+        <h1>ONE</h1>
+      </div>
+      <div className="two">
+        <h1 className="text2">TWO</h1>
+      </div>
+      <div className="three">
+        <h1 className="text3">THREE</h1>
+      </div>
     </div>
   );
 }
